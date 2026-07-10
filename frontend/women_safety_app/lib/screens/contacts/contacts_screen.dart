39d28c0b8568af130cb4a81317608fadd1cb8584
admin <namratadalvi2005@gmail.com>
2026-07-10 18:53:25 +0530

    updated

--- frontend/women_safety_app/lib/screens/contacts/contacts_screen.dart
+++ frontend/women_safety_app/lib/screens/contacts/contacts_screen.dart
@@ -235,7 +235,7 @@ class _ContactsScreenState extends State<ContactsScreen> {
                       ],
                     ),
                   ),
-                );git 
+                );
               },
             ),
 
