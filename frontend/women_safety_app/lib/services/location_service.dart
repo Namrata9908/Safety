3ddc2bdbe4eec43aabfb0bba4f53f463e1875563
admin <namratadalvi2005@gmail.com>
2026-07-10 20:49:11 +0530

    ata registration cha feature add karnar ahe

--- frontend/women_safety_app/lib/services/location_service.dart
+++ frontend/women_safety_app/lib/services/location_service.dart
@@ -2,30 +2,38 @@ import 'package:geolocator/geolocator.dart';
 
 class LocationService {
   static Future<Position> getCurrentLocation() async {
-    // Check if location service is enabled
+    // Check location service
     bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
 
     if (!serviceEnabled) {
-      throw Exception("Location services are disabled.");
+      throw Exception(
+        "Location service is OFF. Please enable GPS and try again.",
+      );
     }
 
     // Check permission
     LocationPermission permission = await Geolocator.checkPermission();
 
+    // Request permission
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
     }
 
+    // User denied permission
     if (permission == LocationPermission.denied) {
-      throw Exception("Location permission denied.");
-    }
-
-    if (permission == LocationPermission.deniedForever) {
       throw Exception(
-        "Location permission permanently denied. Please enable it from Settings.",
+        "Location permission is required for SOS. Please allow location access.",
       );
     }
 
+    // User selected Don't ask again
+    if (permission == LocationPermission.deniedForever) {
+      throw Exception(
+        "Location permission permanently denied. Please enable it from App Settings.",
+      );
+    }
+
+    // Get current location
     return await Geolocator.getCurrentPosition(
       desiredAccuracy: LocationAccuracy.high,
     );
